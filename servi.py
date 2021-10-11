f95dc12af7325395aaa3efc19aca82506fe39d4a
Daniel Toledo Moreno <danieltoledo790@gmail.com>
2021-10-11 08:19:36 -0500

    a

--- servi.py
+++ servi.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 PORT=5000
 DEBUG=False
 
 @app.route('/')
 def alguien_entro_a_la_ruta():
-	return 'Funciona!!!!'
+	return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(port=PORT, debug=DEBUG)
