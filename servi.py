1242f537a97cc3af9a286c2762c49641866c63a5
Daniel Toledo Moreno <danieltoledo790@gmail.com>
2021-10-11 01:07:02 -0500

    q

--- servi.py
+++ servi.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 app = Flask(__name__)
+PORT=5000
+DEBUG=False
 
 @app.route('/')
 def alguien_entro_a_la_ruta():
 	return 'Funciona!!!!'
 
-if __name__ == "__servi__":
-    app.run()
+if __name__ == "__main__":
+    app.run(port=PORT, debug=DEBUG)
